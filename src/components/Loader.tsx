f1d0a630a035cc79fd7d35a3a04a0b4421bf3ba4
Mouhealdien <mhyaldynalamyn45@gmail.com>
2024-06-11 01:44:45 +0300

    adding loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,17 +1,25 @@
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 
 const Loader = () => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-
   return (
-    <div id="load">
-      <div className="inline">
+    <div
+      dir="ltr"
+      className="loading  bg-transparent space-x-1 z-50 text-[1rem] sm:text-[6rem]  text-primary loading01"
+    >
+      <span>B</span>
+      <span>E</span>
+      <span>Y</span>
+      <span>O</span>
+      <span>N</span>
+      <span>D</span>
+      <span className="inline">
         <svg
-          className="inline ml-0 pl-0 mt-[23px] md:mt-[27px]    w-[10px] h-[14px] md:w-[15px] md:h-[20px] "
+          className="inline ml-0 pl-0 mt-[23px] md:mt-[27px]    w-[15px] h-[14px] md:w-[16px] md:h-[20px] "
           width="15"
           height="20"
           viewBox="0 0 23 22"
-          fill="#1479FF"
+          fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
@@ -19,13 +27,7 @@ const Loader = () => {
             fill="#1479FF"
           />
         </svg>
-      </div>
-      <div>D</div>
-      <div>N</div>
-      <div>O</div>
-      <div>Y</div>
-      <div>E</div>
-      <div>B</div>
+      </span>
     </div>
   );
 };
