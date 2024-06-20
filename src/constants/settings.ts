9fec6048ec322e571bbf7192f8f155a5a83b6a70
Mouhealdien <mhyaldynalamyn45@gmail.com>
2024-06-20 15:40:12 +0300

    adding render backend link and ignore typscript build errors

--- src/constants/settings.ts
+++ src/constants/settings.ts
@@ -1,2 +1,3 @@
-export const SERVER_URL = 'http://127.0.0.1:8080';
+//export const SERVER_URL = 'http://127.0.0.1:8080';
+export const SERVER_URL = 'https://beyondbackend-3.onrender.com/';
 export const BASE_API_URL = SERVER_URL ;
