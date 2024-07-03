8499c6b78df37b9d52aaff5add4059ffcc4ddb17
Mouhealdien <mhyaldynalamyn45@gmail.com>
2024-07-04 01:05:31 +0300

    add image src for next config and add backend link

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,35 @@ const nextConfig = {
     typescript: {
 
         ignoreBuildErrors: true,
-    }
+    },
+    images: {
+        domains: ['server.beyond4m.com'],
+        remotePatterns: [{
+                protocol: 'http',
+                hostname: '127.0.0.1',
+                // port: '',
+                // pathname: '/account123/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'efac-5-193-21-208.ngrok-free.app',
+                // port: '',
+                // pathname: '/account123/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'beyondbackend-3.onrender.com',
+                // port: '',
+                // pathname: '/account123/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'server.beyond4m.com',
+                // port: '',
+                // pathname: '/account123/**',
+            }
+        ],
+    },
 };
 
 export default nextConfig;
--- src/constants/settings.ts
+++ src/constants/settings.ts
@@ -1,3 +1,4 @@
 //export const SERVER_URL = 'http://127.0.0.1:8080';
-export const SERVER_URL = 'https://beyondbackend-3.onrender.com/';
+//export const SERVER_URL = 'https://beyondbackend-3.onrender.com/';
+export const SERVER_URL = "https://server.beyond4m.com/";
 export const BASE_API_URL = SERVER_URL ;
